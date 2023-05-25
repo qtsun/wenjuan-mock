ad8acdd88c95f3396b27a0886415865c0763e89e
qtsun <838280705@qq.com>
2023-05-25 21:44:18 +0800

    一些接口

---
+++ mock/question.js
@@ -0,0 +1,85 @@
+const Mock = require('mockjs')
+const getQuestionList = require('./data/getQuestionList')
+const Random = Mock.Random
+module.exports = [
+    {   
+        //获取单个问卷信息
+        url:'/api/question/:id',
+        method:'get',
+        response() {
+            return {
+                errno: 0,
+                data:{
+                    id:Random.id(),
+                    title:Random.ctitle()
+                }
+                // errno:122,
+                // msg:'错误'
+            }
+        }
+    },
+    // 创建问卷
+    {
+        url:'/api/question',
+        method:'post',
+        response() {
+            return {
+                errno: 0,
+                data:{
+                    id:Random.id(),
+                }
+            }
+        }
+    },
+    // 获取（查询）问卷列表
+    {
+        url:'/api/question',
+        method:'get',
+        response(ctx){
+            const {url = '', query = {}} = ctx
+            const isDeleted = url.indexOf('isDeleted=true')>=0
+            const isStar = url.indexOf('isStar=true')>=0
+            const pageSize = parseInt(query.pageSize) || 10
+            return {
+                errno:0,
+                data:{
+                    list:getQuestionList({len: pageSize,isDeleted, isStar}),
+                    total:100
+                }
+            }
+        }
+    },
+    {
+        //更新问卷
+        url:'/api/question/:id',
+        method:'patch',
+        response(){
+            return {
+                errno: 0
+            }
+        }
+    },
+    {
+        // 复制问卷
+        url: '/api/question/duplicate/:id',
+        method: 'post',
+        response(){
+            return {
+                errno: 0,
+                data:{
+                    id:Random.id()
+                }
+            }
+        }
+    },
+    {
+        //批量彻底删除
+        url:'/api/question',
+        method:'delete',
+        response(){
+            return {
+                errno: 0
+            }
+        }
+    }
+]
