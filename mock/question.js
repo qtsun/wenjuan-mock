6c1f8504d3a148c6e45c0d0a6031e1907a44966a
qtsun <838280705@qq.com>
2023-06-05 21:29:54 +0800

    add checkboc and radio

--- mock/question.js
+++ mock/question.js
@@ -13,11 +13,22 @@ module.exports = [
                     id:Random.id(),
                     title:Random.ctitle(),
                     componentList:[
+                        // Info
+                        {
+                            fe_id: Random.id(),
+                            type: 'questionInfo',
+                            title:'问卷信息',
+                            isHidden: false,
+                            isLocked:false,
+                            props:{title:'问卷标题', desc:'问卷描述'}
+                        },
                         // Title
                         {
                             fe_id: Random.id(),
                             type:'questionTitle', // 组件类型不能重复,前后端统一好
                             title:'标题',
+                            isHidden: false,
+                            isLocked:false,
                             props:{text:'个人信息调研', level:1, isCenter: false}
                         },
                         // Input
@@ -25,6 +36,8 @@ module.exports = [
                             fe_id: Random.id(),
                             type: 'questionInput',
                             title:'输入框',
+                            isHidden: false,
+                            isLocked:false,
                             props:{title:'你的姓名', placeholder:'请输入姓名...'}
                         },
                         // Input
@@ -32,7 +45,62 @@ module.exports = [
                             fe_id: Random.id(),
                             type: 'questionInput',
                             title:'输入框2',
+                            isHidden: false,
+                            isLocked:false,
                             props:{title:'你的电话', placeholder:'请输入电话...'}
+                        },
+                        // Textarea
+                        {
+                            fe_id: Random.id(),
+                            type: 'questionTextarea',
+                            title:'多行输入',
+                            isHidden: false,
+                            isLocked:false,
+                            props:{title:'你的爱好', placeholder:'请输入...'}
+                        },
+                        // Paragraph
+                        {
+                            fe_id: Random.id(),
+                            type: 'questionParagraph',
+                            title:'段落',
+                            isHidden: false,
+                            isLocked:false,
+                            props:{text:'一行段落', isCenter:false}
+                        },
+                        // Radio
+                        {
+                            fe_id: Random.id(),
+                            type: 'questionRadio',
+                            title:'单选',
+                            isHidden: false,
+                            isLocked:false,
+                            props:{
+                                title:'单选标题',
+                                isVertical:false,
+                                options: [
+                                    { value: 'item1', text: '选项1' },
+                                    { value: 'item2', text: '选项2' },
+                                    { value: 'item3', text: '选项3' },
+                                  ],
+                                  value: '',
+                            }
+                        },
+                         // Checkbox
+                         {
+                            fe_id: Random.id(),
+                            type: 'questionCheckbox',
+                            title:'多选',
+                            isHidden: false,
+                            isLocked:false,
+                            props:{
+                                title: '多选标题',
+                                isVertical: false,
+                                list: [
+                                    { value: 'item1', text: '选项1', checked: false },
+                                    { value: 'item2', text: '选项2', checked: false },
+                                    { value: 'item3', text: '选项3', checked: false },
+                                ],
+                            }
                         }
                     ]
                 }
